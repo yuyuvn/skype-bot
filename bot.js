a5bef71ed36dbf4babeb36670f8c6386d6ba61c5
Clicia Scarlet <yuyuvn@mac.com>
2016-04-28 13:39:39 +0700

    Init

---
+++ bot.js
@@ -0,0 +1,22 @@
+'use strict';
+const uuid = require('node-uuid');
+const inspect = require('util').inspect;
+const skype = require('skype-sdk');
+class Bot {
+  constructor(botService) {
+    botService.onIncomingCall(this.onIncomingCall.bind(this));
+  }
+
+  onIncomingCall(incomingCall, workflow, callback) {
+    debug('New incoming call:\n' + inspect(incomingCall, { depth: 5 }));
+
+    workflow.actions = [
+      new skype.Answer({
+          operationId: uuid.v4()
+      })
+    ];
+
+    callback(null, workflow);
+  }
+}
+module.exports = Bot;
---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+const restify = require('restify');
+const skype = require('skype-sdk');
+const Bot = require('./bot');
+const botService = new skype.BotService({
+  calling: {
+    callbackUri: 'https://skype-answer-bot.azurewebsites.net/v1/callback'
+  }
+});
+const bot = new Bot(botService);
+const server = restify.createServer();
+server.use(skype.ensureHttps(true));
+server.use(skype.verifySkypeCert());
+server.post('/v1/calls', skype.incomingCallHandler(botService));
+server.post('/v1/callback', skype.incomingCallbackHandler(botService));
+const port = process.env.PORT || 8000;
+server.listen(port);
+console.log('listening on port ' + port);
